43180478d927d430f009984a4ccf4fc174718f63
Eric Larsen <eric.larsen@pingsolutionsgroup.com>
2021-10-21 21:07:48 -0400

    Used for Sentiment Analysis for Talent Management

--- PythonWordCloud.py
+++ PythonWordCloud.py
@@ -9,24 +9,26 @@ import stylecloud
 
 #Insert the additional stop words here#
 stop_words = get_stop_words('english')
-stop_words.append('MRO')
+stop_words.append('Officer')
 stop_words.append('Marine')
-stop_words.append('Concurs')
+stop_words.append('Corps')
 stop_words.append('Marines')
-stop_words.append('Peer')
-stop_words.append('Concur')
-stop_words.append('Peers')
-stop_words.append('Continue'
-stop_words.append('RS')
-stop_words.append('Detachment')
-stop_words.append('Challenge')
-stop_words.append('Sgt')
 stop_words.append('Can')
+stop_words.append('S')
+stop_words.append('can')
+stop_words.append('Officers')
+##stop_words.append('RS')
+##stop_words.append('Detachment')
+##stop_words.append('Challenge')
+##stop_words.append('Sgt')
+##stop_words.append('Can')
 
 
 #Website to choose the design of the cloud https://fontawesome.com/icons?d=gallery&p=2&m=free#
 
 #Can remove palette command below to use default colors.  Chose pallete from https://jiffyclub.github.io/palettable/
 
+#palette='colorbrewer.diverging.RdBu_11'
 
-stylecloud.gen_stylecloud(file_path='comments.txt', icon_name= "fas fa-cloud", palette='colorbrewer.diverging.RdBu_11', custom_stopwords=stop_words) #Icon name is where you can go to the above website to change the style of the word cloud
+
+stylecloud.gen_stylecloud(file_path='Talent.txt', icon_name= "fas fa-cloud", palette='colorbrewer.diverging.RdBu_11', custom_stopwords=stop_words) #Icon name is where you can go to the above website to change the style of the word cloud
